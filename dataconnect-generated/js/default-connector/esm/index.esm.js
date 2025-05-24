63b117f2d7dc85a422bd3d9ecbdcf6e12de61b48
omprakashsuthar <omprakashsuhar@gmail.com>
2025-05-24 15:47:28 +0530

    Initial commit with Firebase setup

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'studentteacherappointmentbooking',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'studentteacherappointmentbooking',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
